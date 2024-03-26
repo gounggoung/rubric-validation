a9ba49c7d23a9c7d8f7f284e522119dbf0032bab
Jacob Johnston <69246670+gounggoung@users.noreply.github.com>
2024-03-26 21:53:32 +1100

    Extract learning outcomes from csv

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import csv
 
 filename = input("Enter the file name: ")
+unit_learning_outcomes = []
 
-with open(filename) as rubric:
-    lines = []
-    for line in rubric:
-        lines.append(line)
+with open(filename, newline='') as unit_guide:
+    unit_guide_reader = csv.DictReader(unit_guide, delimiter=';')
+    for row in unit_guide_reader:
+        assessment_outcomes = str(row['Learning Outcomes']).split(',')
+        for outcome in assessment_outcomes:
+            outcome = outcome.strip()
+            if outcome not in unit_learning_outcomes:
+                unit_learning_outcomes.append(outcome)
+
+print(len(unit_learning_outcomes))
+
+
+
 
 
 
