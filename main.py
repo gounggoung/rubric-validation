8b623c232664ecb8faf40e92c2edbeb7254867b1
Jacob Johnston <69246670+gounggoung@users.noreply.github.com>
2024-04-08 15:46:31 +1000

    Read in assessment task information

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import csv
 import spacy
 from bloomlevel import Bloom_Level
 from outcome import Outcome
+from assessmenttask import Assessment_Task
 
 
 nlp = spacy.load('en_core_web_trf')
@@ -9,6 +10,7 @@ nlp = spacy.load('en_core_web_trf')
 filename = input("Enter the file name: ")
 unit_learning_outcomes = []
 bloom_levels = {}
+assessment_tasks = []
 
 
 
@@ -18,11 +20,18 @@ def read_bloom_verbs(level, weight):
 
 
 # Open CSV containing unit guide information
-# Read all unique outcomes into list
+
 with open(filename, newline='') as unit_guide:
     unit_guide_reader = csv.DictReader(unit_guide)
+
+
+    # Read all unique outcomes into list
     for row in unit_guide_reader:
         assessment_outcomes = str(row['Learning Outcomes']).split(';')
+
+        # Read in assessment task information, and string representation of outcomes
+        assessment_tasks.append(Assessment_Task(row['Assessment Task'], row['Weighting'], row['Hurdle'], row['Due Date'], assessment_outcomes))
+
         for outcome in assessment_outcomes:
             outcome = outcome.strip()
             # Only add unique outcomes not already in the list
@@ -52,10 +61,21 @@ while outcome_index < len(unit_learning_outcomes):
                     unit_learning_outcomes[outcome_index] = Outcome(outcome, bloom_levels[level], bloom_levels[level].weight, outcome_index)
                     break
     outcome_index += 1
-                
+
+# Repalce str represenation of outcomes in assessment task with class version
+for assessment in assessment_tasks:
+    assessment_outcome_index = 0
+    while assessment_outcome_index < len(assessment.outcomes):
+        for outcome in unit_learning_outcomes:
+            if outcome.outcome_description == assessment.outcomes[assessment_outcome_index]:
+                assessment.outcomes[assessment_outcome_index] = outcome
+                break
+        assessment_outcome_index += 1           
 
 for outcome in unit_learning_outcomes:
     print(outcome)
+
+
 
 
 
