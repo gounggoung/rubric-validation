91b18930982b6f44b84155949040139afbb9897b
Jacob Johnston <69246670+gounggoung@users.noreply.github.com>
2024-04-08 13:58:41 +1000

    Read in weight value

--- bloomlevel.py
+++ bloomlevel.py
@@ -3,6 +3,8 @@ class Bloom_Level:
     def __init__(self, verb_list, level_weight):
         with open(verb_list) as verbs:
             self.verb_list = verbs.readline().split(',')
+            
+        self.weight = level_weight
 
 
     def __repr__(self):
