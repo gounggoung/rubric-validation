3b1e37dee7b29e4f738bb3e6afc5876f74bf3500
Jacob Johnston <69246670+gounggoung@users.noreply.github.com>
2024-04-08 15:46:20 +1000

    Add constructor for Assessment_Task

--- assessmenttask.py
+++ assessmenttask.py
@@ -1,2 +1,17 @@
 class Assessment_Task:
-    pass
+    
+    def __init__(self, task_name, weighting, hurdle, due_date, outcomes):
+        self.task_name = task_name
+        self.weighting = weighting
+        
+        if(hurdle == 'Yes'):
+            self.hurdle = True
+        else:
+            self.hurdle = False
+
+        
+        self.due_date = due_date
+        self.outcomes = outcomes
+
+    def __repr__(self):
+        return "{task_name}     {weighting}     {hurdle}    {due_date}".format(task_name = self.task_name, weighting = self.weighting, hurdle = self.hurdle, due_date = self.due_date)
