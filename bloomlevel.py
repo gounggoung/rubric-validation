d7408596291654b323807298474257554a4d9471
Jacob Johnston <69246670+gounggoung@users.noreply.github.com>
2024-04-08 13:43:35 +1000

    Add constructor and string repr

--- bloomlevel.py
+++ bloomlevel.py
@@ -1,3 +1,10 @@
 class Bloom_Level:
-    pass
+    
+    def __init__(self, verb_list, level_weight):
+        with open(verb_list) as verbs:
+            self.verb_list = verbs.readline().split(',')
+
+
+    def __repr__(self):
+        return str(self.verb_list)
     
