8acd107710fce8dcd69d52d099de55f5665cf5c1
Jacob Johnston <69246670+gounggoung@users.noreply.github.com>
2024-04-08 15:04:51 +1000

    add level name field

--- bloomlevel.py
+++ bloomlevel.py
@@ -1,12 +1,13 @@
 class Bloom_Level:
     
-    def __init__(self, verb_list, level_weight):
+    def __init__(self, verb_list, level_name, level_weight):
         with open(verb_list) as verbs:
             self.verb_list = verbs.readline().split(',')
-            
+
+        self.level_name = level_name
         self.weight = level_weight
 
 
     def __repr__(self):
-        return str(self.verb_list)
+        return str(self.level_name)
     
